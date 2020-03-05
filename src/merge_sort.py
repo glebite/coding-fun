5cadd76ea3ecb8fd12c73ccdcb6dd88455cd967d
Lazlo <glebite@gmail.com>
2020-03-04 21:01:59 -0500

    Added homogenous type check

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -2,13 +2,24 @@
 merge_sort.py
 """
 
-def merge(array): 
+def homogenous_type(array):
+    """ homogenous_type """
+    if not array:
+        return False
+    element_type = type(array[0])
+    for element in array[1:]:
+        if type(element) is not element_type:
+            return False
+    return True
+
+def merge(array):
+    """ merge """
     if len(array) >1: 
         mid = len(array)//2 
         left_side = array[:mid] ; right_side = array[mid:]
   
         merge(left_side)  ; merge(right_side)
-  
+       
         i = j = k = 0
           
         while i < len(left_side) and j < len(right_side): 
@@ -23,4 +34,3 @@ def merge(array):
           
         while j < len(right_side): 
             array[k] = right_side[j] ; j+=1 ; k+=1
-
