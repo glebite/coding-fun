e229f9b428fd97b13a956a577e13f143fc43554e
Lazlo <glebite@gmail.com>
2020-03-17 20:54:47 -0400

    fleshing out the code...  yay!

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -3,11 +3,39 @@ binary_tree
 """
 
 class Node:
+    """ Node """
     def __init__(self, value=None):
+        """ __init__ """
         self.value = value
         self.left = None
         self.right = None
 
+class Tree:
+    """ Tree """
+    def __init__(self):
+        self.root = None
+
+    def getRoot(self):
+        return self.root
+
+    def add_node(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        else:
+            self._add_node(value, self.root)
+
+    def _add_node(self, value, node):
+        if val < node.value:
+            if node.left = None:
+                self._add(value, node.left)
+            else:
+                node.left = Node(value)
+        else:
+            if node.right != None:
+                self._add(value, node.right)
+            else:
+                node.right = Node(value)
+    
 def main():
     pass
 
