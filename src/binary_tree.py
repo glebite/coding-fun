42a8cf032cb5ea548b0125d371093d6e20cead42
Lazlo <glebite@gmail.com>
2020-03-20 23:22:47 -0400

    adding doc strings and starting to do a tree to list conversion thing...

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -13,18 +13,22 @@ class Node:
 class Tree:
     """ Tree """
     def __init__(self):
+        """ __init__ """
         self.root = None
 
     def get_root(self):
+        """  get_root """
         return self.root
 
     def add_node(self, value):
+        """ add_node """
         if self.root == None:
             self.root = Node(value)
         else:
             self._add_node(value, self.root)
 
     def _add_node(self, value, node):
+        """ _add_node """
         if value < node.value:
             if node.left != None:
                 self._add_node(value, node.left)
@@ -37,21 +41,28 @@ class Tree:
                 node.right = Node(value)
                 
     def display(self):
+        """ display """
         if self.root != None:
             self._print(self.root)
 
     def _print(self, node):
+        """ _print """
+        nodes = list()
         if node != None:
             self._print(node.left)
             print(str(node.value))
+            nodes.append(str(node.value))
             self._print(node.right)
-
+        return
+            
 def main():
+    """ main """
     x = Tree()
     x.add_node(3)
     x.add_node(5)
     x.add_node(1)
-    x.display()
+    print(x.display())
+    
 
 if __name__ == "__main__":
     main()
