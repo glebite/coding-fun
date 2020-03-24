eb621fa697a25908d42527b2444c3283f9d35f79
Lazlo <glebite@gmail.com>
2020-03-23 21:58:14 -0400

    added list conversion test

--- tests/test_binary_tree.py
+++ tests/test_binary_tree.py
@@ -8,3 +8,10 @@ def test_basic_add_one():
     x = Tree()
     x.add_node(5)
     assert x.root.value == 5
+
+def test_basic_add_two_to_list():
+    x = Tree()
+    x.add_node(1)
+    x.add_node(3)
+    assert x.to_list() == [1,3]
+
