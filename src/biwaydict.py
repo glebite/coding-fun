b21b50a306ee6550b5a5f6fcd01313f6215afa7f
Lazlo <glebite@gmail.com>
2020-03-24 20:00:28 -0400

    putting together some coding fun...

---
+++ src/biwaydict.py
@@ -0,0 +1,15 @@
+"""
+bieaydict.py
+
+- an interesting idea of something in my mind...
+
+"""
+class Biwaydict(dict):
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    x = Biwaydict()
+
+    x['what'] = "something"
+    print(x)
