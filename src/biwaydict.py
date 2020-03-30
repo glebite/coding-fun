609d44ebee72c6e8a0d6370d559e19d6da92d8b4
Lazlo <glebite@gmail.com>
2020-03-29 20:12:12 -0400

    starting into some code - not quite liking how one site described it...

--- src/biwaydict.py
+++ src/biwaydict.py
@@ -8,6 +8,15 @@ class Biwaydict(dict):
     def __init__(self):
         pass
 
+    def __setitem__(self, key, value):
+        if key in self:
+            del self[self[key]]
+        if value in self:
+            del self[value]
+        super().__setitem__(key, value)
+        super().__setitem__(value, key)
+
+
 if __name__ == "__main__":
     x = Biwaydict()
 
