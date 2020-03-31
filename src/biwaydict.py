e8c2f16a6437224dbc0aa5b9592d42d5e520a08c
Lazlo <glebite@gmail.com>
2020-03-30 21:43:08 -0400

    redoing to my original model...

--- src/biwaydict.py
+++ src/biwaydict.py
@@ -4,21 +4,23 @@ bieaydict.py
 - an interesting idea of something in my mind...
 
 """
-class Biwaydict(dict):
+class Biwaydict:
     def __init__(self):
-        pass
+        self.forward = dict()
+        self.backward = dict()
 
-    def __setitem__(self, key, value):
-        if key in self:
-            del self[self[key]]
-        if value in self:
-            del self[value]
-        super().__setitem__(key, value)
-        super().__setitem__(value, key)
-
+    def add(self, key, value):
+        self.forward[key] = value
+        self.backward[value] = key
 
 if __name__ == "__main__":
     x = Biwaydict()
 
-    x['what'] = "something"
-    print(x)
+    x.add('a', 'something')
+
+    print(x.forward)
+    print(x.backward)
+    print(x.forward.keys())
+    
+    print(x.backward.keys())
+    
