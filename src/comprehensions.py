67b488db034b5687db10ff515ae9d6bb1f5282fe
glebite <glebite@gmail.com>
2021-04-29 20:52:16 -0400

    Some more examples for legacy sake...

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -1,4 +1,5 @@
-list_test = [i for i in range(10) if i % 2]
+# this is why we do list comprehensions - it is so much more readable
+list_test = [integer for integer in range(10) if integer % 2]
 print(list_test)
 
 dict_test = {key: value for key, value in enumerate(list_test)}
@@ -6,3 +7,12 @@ print(dict_test)
 
 dict_test = {value: key for key, value in enumerate(list_test)}
 print(dict_test)
+
+set_creation = {var for var in list_test}
+print(set_creation)
+print(type(set_creation))
+
+comp_generator = (var for var in list_test)
+for var in comp_generator:
+    print(var, end=' ')
+print
