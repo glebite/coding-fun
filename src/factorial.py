aa65358dc36aa8039f6670ed1d0ab4c1218b7f13
glebite <glebite@gmail.com>
2021-05-01 21:55:13 -0400

    cleaning up stuff.

--- src/factorial.py
+++ src/factorial.py
@@ -4,9 +4,14 @@ factorial.py
 
 From Wikipedia:
 
-In mathematics, the factorial of a positive integer n, denoted by n!, is the product of all positive integers less than or equal to n:
+In mathematics, the factorial of a positive integer n,
+denoted by n!, is the product of all positive integers
+less than or equal to n:
 
-{\displaystyle n!=n\times (n-1)\times (n-2)\times (n-3)\times \cdots \times 3\times 2\times 1\,.}{\displaystyle n!=n\times (n-1)\times (n-2)\times (n-3)\times \cdots \times 3\times 2\times 1\,.}
+{\displaystyle n!=n\times (n-1)\times (n-2)\times (n-3)
+\times \cdots \times 3\times 2\times 1\,.}{\displaystyle 
+n!=n\times (n-1)\times (n-2)\times (n-3)\times
+\cdots \times 3\times 2\times 1\,.}
 For example,
 
 {\displaystyle 5!=5\times 4\times 3\times 2\times 1=120\,.}{\displaystyle 5!=5\times 4\times 3\times 2\times 1=120\,.}
@@ -18,8 +23,8 @@ factorial N:
     if N is 0,
         return 1 (factorial of 0 = 1)
     return N * factorial of N -1
-
 """
+
 
 def factorial(n):
     """
@@ -32,4 +37,3 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-
